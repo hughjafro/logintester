64f4523fb1ac5036d479e493ccf0ddb85f0dd462
Christopher Tolbert <cmtolbert1@gmail.com>
2013-11-21 21:23:12 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Logintester::Application.config.secret_key_base = 'ef529fd586c72b61bc8e72b2acdc54be90031ff55b529f2cb351c59fe20561c66baa8dfb5dd0d1ef38e63f1b720ef42e22644b9ac669901726284b3bf0c65d63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Logintester::Application.config.session_store :cookie_store, key: '_logintester_session'
